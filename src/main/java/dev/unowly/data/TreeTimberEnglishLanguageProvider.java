e8ad7c2c77a42dafed9a55cdefc94b1ba2a0033d
unowly <mail@unowly.dev>
2025-06-11 12:37:16 +0200

    add translation en_us

---
+++ src/main/java/dev/unowly/data/TreeTimberEnglishLanguageProvider.java
@@ -0,0 +1,19 @@
+package dev.unowly.data;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class TreeTimberEnglishLanguageProvider extends FabricLanguageProvider {
+    public TreeTimberEnglishLanguageProvider(FabricDataOutput dataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> registryLookup) {
+        super(dataOutput, "en_us", registryLookup);
+    }
+
+    @Override
+    public void generateTranslations(RegistryWrapper.WrapperLookup wrapperLookup, TranslationBuilder translationBuilder) {
+        translationBuilder.add("key.category.treetimber.treetimber", "TreeTimber");
+        translationBuilder.add("key.treetimber.timber", "Toggle TreeTimber");
+    }
+}
