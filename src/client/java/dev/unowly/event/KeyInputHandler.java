3f4aa86b7f4a2d396c1c351124638451082aa838
unowly <mail@unowly.dev>
2025-06-13 16:16:40 +0200

    added keybind function for config

--- src/client/java/dev/unowly/event/KeyInputHandler.java
+++ src/client/java/dev/unowly/event/KeyInputHandler.java
@@ -1,5 +1,7 @@
 package dev.unowly.event;
 
+import dev.unowly.config.TreeTimberConfig;
+import me.shedaniel.autoconfig.AutoConfig;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
@@ -9,13 +11,21 @@ import org.lwjgl.glfw.GLFW;
 public class KeyInputHandler {
     public static final String KEY_CATEGORY_TREE_TIMBER = "key.category.treetimber.treetimber";
     public static final String KEY_TREE_TIMBER = "key.treetimber.timber";
+    public static final String KEY_TREE_TIMBER_CONFIG = "key.treetimber.timber_config";
 
     public static KeyBinding timberKey;
+    public static KeyBinding timberConfigKey;
 
-    public static void registerKeyInputs() {
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {;
+    public static void registerKeyCommandInputs() {
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (timberKey != null && timberKey.wasPressed()) {
-                client.player.networkHandler.sendCommand("timber");
+                if (client.player != null) {
+                    client.player.networkHandler.sendCommand("timber");
+                }
+            }
+
+            if (timberConfigKey != null && timberConfigKey.wasPressed()) {
+                client.setScreen(AutoConfig.getConfigScreen(TreeTimberConfig.class, client.currentScreen).get());
             }
         });
     }
@@ -28,6 +38,13 @@ public class KeyInputHandler {
                 KEY_CATEGORY_TREE_TIMBER
         ));
 
-        registerKeyInputs();
+        timberConfigKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                KEY_TREE_TIMBER_CONFIG,
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_G,
+                KEY_CATEGORY_TREE_TIMBER
+        ));
+
+        registerKeyCommandInputs();
     }
 }
